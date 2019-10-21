b84da357ec5d21cb49f9dbf255ca09892d30bfa4
dotcasek <dotcasek@gmail.com>
2019-10-21 14:53:24 -0400

    monday class

---
+++ 06/robot.js
@@ -0,0 +1,23 @@
+Robot = function(){
+  this.head = new THREE.Bone();
+  this.head.position.y = -55;
+
+  this.neck = new THREE.Bone();
+  this.neck.position.y = -10;
+
+  this.head.add(this.neck);
+
+  this.torso = new THREE.Bone();
+  this.torso.position.y = -10;
+
+  this.neck.add(this.torso);
+
+  this.left_lower_arm = null;
+  this.left_upper_arm = new THREE.Bone();
+  this.left_upper_arm.position.x = 10;
+  this.left_hand = null;
+
+  this.left_upper_leg = null;
+  this.left_lower_leg = null;
+  this.left_foor = null;
+};
