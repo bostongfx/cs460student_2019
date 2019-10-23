f1f0f0a27d9c8c267591de3e1c1ee2dcf5553cfe
Jacob Yoon <tbontb24@gmail.com>
2019-10-23 14:59:42 -0400

    Updated to deal with slerp

--- 06/robot.js
+++ 06/robot.js
@@ -1,50 +1,4 @@
 Robot = function( x , y , z ) {
-
-Robot.prototype.show = function(scene) {
-
-  var rGroup = new THREE.Group();
-  rGroup.add( this.head );
-
-  var helper = new THREE.SkeletonHelper( rGroup );
-  helper.material.linewidth = 10; // make the skeleton thick
-
-  scene.add(rGroup);
-  scene.add(helper);
-
-};
-
-Robot.prototype.raise_left_arm = function() {
-	this.movement = 'raise left arm';
-	console.log("This was raise left arm");
-	};
-
-Robot.prototype.lower_left_arm = function() {
-	this.movement = 'lower left arm';
-	console.log("This was lower left arm");
-	};
-
-Robot.prototype.kick = function() {
-	this.movement = 'kick';
-	console.log("This was a kick");
-	};
-
-Robot.prototype.onAnimate = function() {
-
-	if (this.movement == 'raise left arm') {
-		console.log("RLA");
-		// ... TODO slerping
-		// Move left arm up
-	} else if (this.movement == 'lower left arm') {
-		console.log("LLA");
-		// ... TODO slerping
-		// Move left arm down
-	} else if (this.movement == 'kick') {
-		console.log("KICK");
-		// ... TODO slerping and check once it is done for a backwards slerp
-		// you can use the identity quaternion for a backwards slerp
-	}
-};
-
 // head
 this.head = new THREE.Bone();
 this.head.position.set( x, y, z );
@@ -58,6 +12,8 @@ this.torso = new THREE.Bone();
 this.torso.position.y = -30;
 
 this.neck.add(this.torso);
+
+// TODO add left shoulders?
 
 // left arm
 this.left_upper_arm = new THREE.Bone();
@@ -76,6 +32,8 @@ this.left_hand.position.x = 5;
 this.left_hand.position.y = -15;
 
 this.left_lower_arm.add(this.left_hand);
+
+// TODO add right shoulders?
 
 // right arm
 this.right_upper_arm = new THREE.Bone();
@@ -134,5 +92,77 @@ this.right_foot.position.y = -15;
 
 this.right_lower_leg.add(this.right_foot);
 
+this.movement = null;
+
 };
 
+Robot.prototype.show = function(scene) {
+
+  var rGroup = new THREE.Group();
+  rGroup.add( this.head );
+
+  var helper = new THREE.SkeletonHelper( rGroup );
+  helper.material.linewidth = 10; // make the skeleton thick
+
+  scene.add(rGroup);
+  scene.add(helper);
+
+};
+
+Robot.prototype.onAnimate = function() {
+
+	// console.log("animate");
+
+	if (this.movement == 'raise left arm') {
+		console.log("RLA");
+		// ... TODO slerping
+		// Move left arm up
+	} else if (this.movement == 'lower left arm') {
+		console.log("LLA");
+		// ... TODO slerping
+		// Move left arm down
+	} else if (this.movement == 'kick') {
+		console.log("KICK");
+		// ... TODO slerping and check once it is done for a backwards slerp
+		// you can use the identity quaternion for a backwards slerp
+	}
+};
+
+Robot.prototype.raise_left_arm = function() {
+	this.movement = 'raise left arm';
+	console.log("This was raise left arm");
+	T = Math.PI;
+	r.left_upper_arm.quaternion.x = Math.sin(T/2)
+	r.left_upper_arm.quaternion.y = 0
+	r.left_upper_arm.quaternion.z = 0
+	r.left_upper_arm.quaternion.w = Math.cos(T/2)
+
+	this.left_upper_arm.quaternion.slerp( new THREE.Quaternion(
+		x,
+		y,
+		z,
+		w
+	), 0.1 )
+	};
+
+Robot.prototype.lower_left_arm = function() {
+	this.movement = 'lower left arm';
+	console.log("This was lower left arm");
+	T = Math.PI;
+	r.left_upper_arm.quaternion.x = Math.sin(T/2)
+	r.left_upper_arm.quaternion.y = 0
+	r.left_upper_arm.quaternion.z = 0
+	r.left_upper_arm.quaternion.w = Math.cos(-T/2)
+
+	this.left_upper_arm.quaternion.slerp( new THREE.Quaternion(
+		x,
+		y,
+		z,
+		w
+	), 0.1 )
+	};
+
+Robot.prototype.kick = function() {
+	this.movement = 'kick';
+	console.log("This was a kick");
+	};
