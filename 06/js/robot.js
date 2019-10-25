265d8ba0e358a567d8dca7098ee3ddc443a7ec32
hltdev8642 <hltdev8642@gmail.com>
2019-10-25 09:44:23 -0400

    fixed robot shape

--- 06/js/robot.js
+++ 06/js/robot.js
@@ -1,103 +1,116 @@
-function Robot()
+Robot = function (x, y, z) 
 {
+   this.head = new THREE.Bone();
+   addLimb(this.head, this.face, null, -55);
+   this.head.position.set(x, y, z);
+   
+   this.neck = new THREE.Bone();
+   addLimb(this.neck, this.head, null, -10);
+   
+   this.torso = new THREE.Bone();
+   addLimb(this.torso, this.neck, null, -40)
+   
+   this.right_upper_arm = new THREE.Bone();
+   addLimb(this.right_upper_arm, this.neck, 10, -5);
+   this.left_upper_arm = new THREE.Bone();
+   addLimb(this.left_upper_arm, this.neck, -10, -5);
+   
+   this.right_lower_arm = new THREE.Bone();
+   addLimb(this.right_lower_arm, this.right_upper_arm, 10,-15);
+   this.left_lower_arm = new THREE.Bone();
+   addLimb(this.left_lower_arm, this.left_upper_arm, -10,-15);
+   
+   this.right_hand = new THREE.Bone();
+   addLimb(this.right_hand, this.right_lower_arm, 1,-15);
+   this.left_hand = new THREE.Bone();
+   addLimb(this.left_hand, this.left_lower_arm, -1,-15);
+   
+   this.right_upper_leg = new THREE.Bone();
+   addLimb(this.right_upper_leg, this.torso, 10, -5);   
+   this.left_upper_leg = new THREE.Bone();
+   addLimb(this.left_upper_leg, this.torso, -10, -5);
+   
+   this.right_lower_leg = new THREE.Bone();
+   addLimb(this.right_lower_leg, this.right_upper_leg, 10, -15);   
+   this.left_lower_leg = new THREE.Bone();
+   addLimb(this.left_lower_leg, this.left_upper_leg, -10, -15);
+   
+   this.right_foot = new THREE.Bone();
+   addLimb(this.right_foot, this.right_lower_leg, 1,-15);
+   this.left_foot = new THREE.Bone();
+   addLimb(this.left_foot, this.left_lower_leg, -1 , -15);
 
-    this.head = new THREE.Bone();
-    addLimb(this.head, null, null, -55);
+   function addLimb(object, parent, positionX, positionY) 
+   {
+      this.object = object || new THREE.Bone();
+      this.parent = parent || new THREE.Bone();
+      this.positionX = positionX || null;
+      this.positionY = positionY || null;
+      if (this.positionX != null) 
+      {
+         this.object.position.x = this.positionX;
+      }
+      if (this.positionY != null) 
+      {
+         this.object.position.y = this.positionY;
+      }
+      this.parent.add(this.object)
+   };
 
-    this.neck = new THREE.Bone();
-    addLimb(this.neck, this.head, null, -10);
-  
-    this.torso = new THREE.Bone();
-    addLimb(this.torso, this.neck, null, -30)
-  
-    this.right_upper_arm = new THREE.Bone();
-    addLimb(this.right_upper_arm, this.neck, 10, null);
+   function show(scene) 
+   {
+      rGroup = new THREE.Group();
+      rGroup.add(r.head);
+      scene.add(rGroup);
+      helper = new THREE.SkeletonHelper(rGroup);
+      scene.add(helper);
+   };
 
-    
-    this.right_lower_arm = new THREE.Bone();
-    addLimb(this.right_lower_arm, this.right_upper_arm, 10, null);
+Robot.prototype.show = function(scene) {
 
+  rGroup = new THREE.Group();
+  rGroup.add(r.head);
 
-    this.right_hand = new THREE.Bone();
-    addLimb(this.right_hand, this.right_lower_arm, null, -10);
+  scene.add(rGroup);
 
-    
-    this.right_upper_leg = new THREE.Bone();
-    addLimb(this.right_upper_leg, this.torso, 10,null);
-    
-    this.right_lower_leg = new THREE.Bone();
-    addLimb(this.right_lower_leg, this.right_upper_leg, 10, null);
+  helper = new THREE.SkeletonHelper( rGroup );
 
-    
-    this.right_foot = new THREE.Bone();
-    addLimb(this.right_foot, this.right_lower_leg, null, -10);
-    
+  scene.add(helper);
 
-    this.left_upper_arm = new THREE.Bone();
-    addLimb(this.left_upper_arm, this.neck, -10, null);
+};
 
-    
-    this.left_lower_arm = new THREE.Bone();
-    addLimb(this.left_lower_arm, this.left_upper_arm, -10, null);
-
-
-    this.left_hand = new THREE.Bone();
-    addLimb(this.left_hand, this.left_lower_arm, null, -10);
-
-    
-    this.left_upper_leg = new THREE.Bone();
-    addLimb(this.left_upper_leg, this.torso, -10,null);
-    
-    this.left_lower_leg = new THREE.Bone();
-    addLimb(this.left_lower_leg, this.left_upper_leg, -10, null);
-
-    
-    this.left_foot = new THREE.Bone();
-    addLimb(this.left_foot, this.left_lower_leg, null, -10);
-    
-
-function addLimb(object, parent, positionX, positionY)
+Robot.prototype.raiseLeftArm = function()
  {
-    this.object = object || new THREE.Bone();
-    this.parent = parent || new THREE.Bone();
-    this.positionX = positionX || null;
-    this.positionY = positionY || null;
 
-    if( this.positionX !=null )
-    {
-        this.object.position.x = this.positionX;
-    }
+  this.movement = 'raise left arm';
 
-    if ( this.positionY != null )
-    {
-        this.object.position.y = this.positionY;
-    }
-
-    this.parent.add(this.object)
-    
- }
-/*
- Robot.prototype.raise_left_arm = function() {
-this.movement = 'raise left arm';
 };
 
-Robot.prototype.lower_left_arm = function() {
-this.movement = 'lower left arm';
-};
+Robot.prototype.onAnimate = function()
+ {
 
-Robot.prototype.kick = function() {
-this.movement = 'kick';
-};
+  // gets called on each animate loop
+  // meaning on every frame
 
-Robot.prototype.onAnimate = function() {
-if (this.movement == 'raise left arm') {
-// ... TODO slerping
-} else if (this.movement == 'lower left arm') {
-// ... TODO slerping
-} else if (this.movement == 'kick') {
-// ... TODO slerping and check once it is done for a backwards slerp
-// you can use the identity quaternion for a backwards slerp
+  // check which movement is requested
+  if( this.movement == 'raise left arm')
+   {
+
+    // raise the left arm
+    T=Math.PI;
+    var x = Math.sin(T/2)
+    var y = 0
+    var z = 0
+    var w = Math.cos(T/2)    
+
+    r.left_upper_arm.quaternion.slerp( new THREE.Quaternion(
+                                          x,
+                                          y,
+                                          z,
+                                          w
+      ), 0.1 );
+
+
+  }
 }
 };
-*/
-}
