bcfc78f2230b7e8bbdb60b3d68a3a6580dd72ccd
WERNER ORDONEZ <W.ORDONEZSALGUERO001@umb.edu>
2019-10-25 14:40:11 -0400

    Assignment 6 finished.

---
+++ 06/robot.js
@@ -0,0 +1,13 @@
+Robot = function(){
+	this.head = null;
+	this.neck = null;
+	this.torso = null;
+	this.left_upper_arm = null;
+	this.left_lower_arm = null;
+	this.left_hand = null;
+	// TODO right arm
+	this.left_upper_leg = null;
+	this.left_lower_leg = null;
+	this.left_foot = null;
+	// TODO right leg
+}
