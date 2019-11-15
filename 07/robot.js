dd7891776ee8509b9613d887e1f72a2da4bda42d
Shuhua Liu <shuhua.liu001@umb.edu>
2019-11-15 11:34:08 -0500

    fix merge

--- 07/robot.js
+++ 07/robot.js
@@ -1,6 +1,6 @@
 Robot = function(x, y, z) {
 
-<<<<<<< HEAD
+
   // create head, neck, and torso
   var fromhelper = HELPER.cylinderSkeletonMesh( 3, 5, 'blue');
   var geometry = fromhelper[0];
@@ -153,116 +153,12 @@ Robot.prototype.show = function(scene) {
   scene.add(this.rightleg_mesh);
 
   // ToDo: add other body part meshes
-=======
-	this.head = new THREE.Bone();
-	this.head.position.x = x;
-	this.head.position.y = y; 
-	this.head.position.z = z;
 
-	this.neck = new THREE.Bone();
-	this.neck.position.y = -10;  // relative to the head
-	this.head.add(this.neck);
-
-	this.torso = new THREE.Bone();
-	this.torso.position.y = -30;  // relative to the neck
-	this.neck.add(this.torso);
-
-	// left arm
-	//
-	this.left_upper_arm = new THREE.Bone();
-	this.left_upper_arm.position.x = 10;  // relative to the neck
-	this.left_upper_arm.position.y = -5;
-	this.neck.add(this.left_upper_arm);
-
-	this.left_lower_arm = new THREE.Bone();;
-	this.left_lower_arm.position.x = 10;  // relative to the upper arm
-	this.left_lower_arm.position.y = -15;
-	this.left_upper_arm.add(this.left_lower_arm);
-
-	this.left_hand = new THREE.Bone();
-	this.left_hand.position.x = -1;  // relative to the lower arm
-	this.left_hand.position.y = -5;
-	this.left_lower_arm.add(this.left_hand);
-
-	// right arm
-	//
-	this.right_upper_arm = new THREE.Bone();
-	this.right_upper_arm.position.x = -10;  
-	this.right_upper_arm.position.y = -5;
-	this.neck.add(this.right_upper_arm);
-
-	this.right_lower_arm = new THREE.Bone();;
-	this.right_lower_arm.position.x = -10; 
-	this.right_lower_arm.position.y = -15;
-	this.right_upper_arm.add(this.right_lower_arm);
-
-	this.right_hand = new THREE.Bone();
-	this.right_hand.position.x = 1;  
-	this.right_hand.position.y = -5;
-	this.right_lower_arm.add(this.right_hand);
-
-	// left leg
-	//
-	this.left_upper_leg = new THREE.Bone();
-	this.left_upper_leg.position.x = 8;  // relative to the torso
-	this.left_upper_leg.position.y = -8;
-	this.torso.add(this.left_upper_leg);
-
-	this.left_lower_leg = new THREE.Bone();;
-	this.left_lower_leg.position.x = 5;  // relative to the upper leg
-	this.left_lower_leg.position.y = -15;
-	this.left_upper_leg.add(this.left_lower_leg);
-
-	this.left_foot = new THREE.Bone(); 
-	this.left_foot.position.y = -5;  // relative to the lower leg
-	this.left_lower_leg.add(this.left_foot);
-
-	// right leg
-	//
-	this.right_upper_leg = new THREE.Bone();
-	this.right_upper_leg.position.x = -8;  // relative to the torso
-	this.right_upper_leg.position.y = -8;
-	this.torso.add(this.right_upper_leg);
-
-	this.right_lower_leg = new THREE.Bone();;
-	this.right_lower_leg.position.x = -5;  // relative to the upper leg
-	this.right_lower_leg.position.y = -15;
-	this.right_upper_leg.add(this.right_lower_leg);
-
-	this.right_foot = new THREE.Bone(); 
-	this.right_foot.position.y = -5;  // relative to the lower leg
-	this.right_lower_leg.add(this.right_foot);
-
-	this.left_upper_leg = null;
-
-	this.left_lower_leg = null;
-
-	this.left_foot = null;
-
-	//ToDo: right leg
-
-	// this will control which animation to run
-  	this.movement = null; // for instance 'raise left arm', 
-                        // raises the left arm
-
-
-
-};
-
-Robot.prototype.show = function(scene) {
-
-	rGroup = new THREE.Group();
-	rGroup.add(r.head);
-	scene.add(rGroup);
-
-	helper = new THREE.SkeletonHelper( rGroup );
-	scene.add(helper);
->>>>>>> ef6a2b709506420b6b3aa7c7f455a3959e70b982
 
 };
 
 Robot.prototype.raise_left_arm = function() {
-<<<<<<< HEAD
+
 
   this.movement = 'raise left arm';
 
@@ -284,26 +180,10 @@ Robot.prototype.dance = function() {
   
   this.movement = 'dance';
 
-=======
-	this.movement = 'raise left arm';
-};
-
-Robot.prototype.lower_left_arm = function() {
-	this.movement = 'lower left arm';
-};
-
-Robot.prototype.kick = function(){
-	this.movement = 'kick';
-};
-
-Robot.prototype.dance = function() {
-	this.movement = 'dance';
->>>>>>> ef6a2b709506420b6b3aa7c7f455a3959e70b982
 };
 
 Robot.prototype.onAnimate = function() {
 
-<<<<<<< HEAD
   if (this.movement == 'raise left arm') {
 
     var T = Math.PI;
@@ -399,68 +279,4 @@ Robot.prototype.onAnimate = function() {
   }
 
 };
-=======
-	// gets called on each animate loop
-	// meaning on every frame
 
-	// check which movement is requested
-	if (this.movement == 'raise left arm' ) {
-		// raise the left arm
-		T = -Math.PI;
-		this.left_upper_arm.quaternion.slerp( 
-			new THREE.Quaternion(
-				Math.sin(T/2),
-				0,
-				0,
-				Math.cos(T/2) ), 0.1);
-
-
-	} else if (this.movement == 'lower left arm')  {
-		// lower the left arm
-		this.left_upper_arm.quaternion.slerp( 
-			new THREE.Quaternion(0, 0, 0, 1), 0.1 );
-
-
-	} else if (this.movement == 'kick' ) {
-		if (this.right_upper_leg.quaternion.w < 0.72) {
-			this.movement = 'kick done';
-		} else {
-			T = Math.PI;
-			this.right_upper_leg.quaternion.slerp(
-				new THREE.Quaternion(
-					Math.sin(T/2),
-					0,
-					0,
-					Math.cos( T/2 )), 0.1 );
-		}
-	} else if (this.movement == 'kick done') {
-		this.right_upper_leg.quaternion.slerp( 
-			new THREE.Quaternion(0, 0, 0, 1), 0.1 );
-
-
-	} else if (this.movement == 'dance') {
-
-		if (this.left_upper_arm.quaternion.w < 0.72) {
-			this.movement = 'state1';
-		} else {
-			T = Math.PI;
-			this.left_upper_arm.quaternion.slerp(
-				new THREE.Quaternion(
-					Math.sin(T/2),
-					0,
-					0,
-					Math.cos( T/2 )), 0.1 );
-		}
-
-	} else if (this.movement == 'state1') {
-		this.left_upper_arm.quaternion.slerp( 
-			new THREE.Quaternion(0, 0, 0, 1), 0.1 );
-		this.movement = 'state2';
-
-	}
-}
-
-
-
-
->>>>>>> ef6a2b709506420b6b3aa7c7f455a3959e70b982
