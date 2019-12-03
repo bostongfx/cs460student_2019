f8037a0d21b5a8bde8c5fd6f672235f0b38cc75a
KSHITIJ OZARKAR <k.ozarkar001@umb.edu>
2019-12-02 19:09:15 -0500

    Assignment 10 finished.

--- 10/gltf.py
+++ 10/gltf.py
@@ -1,18 +1,66 @@
 import numpy as np
 import base64
 
-VERTICES = np.array([0.,0.,0.,    0.,1.,0.,    1.,0.,0.], dtype=np.float32)
-INDICES = np.array([0, 1, 2], dtype=np.ushort)
 
-HOWMANY = 3
-MAX_X = 1
-MAX_Y = 1
-MAX_Z = 0
-MIN_X = 0
-MIN_Y = 0
-MIN_Z = 0
-MAX = 2
-MIN = 0
+with open("beethoven.ply", 'r') as f:
+    lines = f.readlines()
+
+counter = 0
+vertices_coming = False
+faces_coming = False
+
+for i,l in enumerate(lines):
+
+    if l.startswith('element vertex'):
+        vertexcounter = int(l.split(' ')[-1].strip())
+    elif l.startswith('element face'):
+        facecounter = int(l.split(' ')[-1].strip())
+    elif l.startswith('end_header'):
+        break
+
+vertex_data = lines[i+1:i+vertexcounter+1]
+face_data = lines[i+vertexcounter+1:]
+
+VERTICES = []
+MAX_X = -np.inf
+MAX_Y = -np.inf
+MAX_Z = -np.inf
+MIN_X = np.inf
+MIN_Y = np.inf
+MIN_Z = np.inf
+
+for v in vertex_data:
+    v = [float(v.strip()) for v in v.strip().split(' ')]
+
+    MAX_X = max(MAX_X, v[0])
+    MAX_Y = max(MAX_Y, v[1])
+    MAX_Z = max(MAX_Z, v[2])
+    MIN_X = min(MIN_X, v[0])
+    MIN_Y = min(MIN_Y, v[1])
+    MIN_Z = min(MIN_Z, v[2])    
+
+    VERTICES += v
+
+INDICES = []
+MAX = -np.inf
+MIN = np.inf
+for i in face_data:
+    i = [int(i) for i in i.strip().split(' ')[1:]]
+    INDICES += i
+
+    MAX = max(MAX, i[0])
+    MAX = max(MAX, i[1])
+    MAX = max(MAX, i[2])
+    MIN = min(MIN, i[0])
+    MIN = min(MIN, i[1])
+    MIN = min(MIN, i[2])
+
+VERTICES = np.array(VERTICES, dtype=np.float32)
+INDICES = np.array(INDICES, dtype=np.ushort)
+
+
+HOWMANY_V = len(VERTICES) / 3 # because its the number of VEC3 groups
+HOWMANY_I = len(INDICES)
 
 HOWMANYBYTES_V = VERTICES.nbytes
 HOWMANYBYTES_I = INDICES.nbytes
@@ -31,7 +79,7 @@ gltf = {
             "bufferView": 0,
             "byteOffset": 0,
             "componentType": 5126,
-            "count": HOWMANY,
+            "count": HOWMANY_V,
             "type": "VEC3",
             "max": [MAX_X, MAX_Y, MAX_Z],
             "min": [MIN_X, MIN_Y, MIN_Z]
@@ -40,7 +88,7 @@ gltf = {
             "bufferView": 1,
             "byteOffset": 0,
             "componentType": 5123,
-            "count": HOWMANY,
+            "count": HOWMANY_I,
             "type": "SCALAR",
             "max": [MAX],
             "min": [MIN]
@@ -103,4 +151,3 @@ gltf = {
 }
 
 print ( str(gltf).replace("'", '"') ) # we need double quotes instead of single quotes
-
